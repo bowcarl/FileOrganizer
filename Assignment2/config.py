d2b41e2eca94a70631dc116fa2ea1740dc3a441e
Carl Petter Mørch-Reiersen <35300302+bowcarl@users.noreply.github.com>
2024-12-01 20:56:20 +0100

    Delete Assignment2 directory

--- Assignment2/config.py
+++
@@ -1,9 +0,0 @@
-CONFIG={}
-
-def add_extention(file_type, extensions, folder):
-    "Add dynamic extention direcotry"
-    CONFIG[file_type] = {
-        "extensions": extensions,
-        "folder": folder
-    }
-    
