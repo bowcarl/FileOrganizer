7ac3f69d393a75e0e9f5036af53e52ef981f35e2
Carl Petter Mørch-Reiersen <35300302+bowcarl@users.noreply.github.com>
2024-12-01 21:01:50 +0100

    Update config.py

--- FileOrganizer/config.py
+++ FileOrganizer/config.py
@@ -1,7 +1,7 @@
 CONFIG={}
 
-def add_extention(file_type, extensions, folder):
-    "Add dynamic extention direcotry"
+def add_extension(file_type, extensions, folder):
+    "Add dynamic extension direcotry"
     CONFIG[file_type] = {
         "extensions": extensions,
         "folder": folder
