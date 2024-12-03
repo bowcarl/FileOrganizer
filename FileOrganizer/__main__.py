7c3b955c04f4296631d507c19f327bb60ee115ac
Carl Petter Mørch-Reiersen <35300302+bowcarl@users.noreply.github.com>
2024-12-03 20:18:36 +0100

    Update __main__.py

--- FileOrganizer/__main__.py
+++ FileOrganizer/__main__.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+# Ensure the parent directory is in the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(file), '../../')))
+sys.path.insert(0, os.path.abspath(os.path.dirname(file)))
 from organize_files import organize_files
 
 def main():
